b3881666a26a53bc188317e25e3d0b812c1f1e81
Jason Kong <jjkong149@hotmail.com>
2024-01-31 19:53:51 -0500

    added time, now shows if multiple instances are up, shortened messages, commented counter reset out

--- src/index.js
+++ src/index.js
@@ -41,9 +41,42 @@ client.on("messageCreate", async (message) => {
   }
 });
 
-counter = 1
-if (counter === 1000000) {
-  counter = 1;
+// Counters
+total_counter = 1; // Total bot uptime
+
+no_counter = 1; // Time that there is no server
+
+yes_counter = 1; // Time that there is a server
+
+// total_days_counter = 0; // Total days that bot is up
+
+instance_counter_tracker = 0; // stores instance value
+
+// Formats the seconds*10 into d, h, m, s
+function formatTime(totalSeconds) {
+  const seconds = totalSeconds * 10;
+  const days = Math.floor(seconds / 864000); // 864000 seconds in a day
+  const hours = Math.floor((seconds % 864000) / 3600); // 3600 seconds in an hour
+  const minutes = Math.floor((seconds % 3600) / 60); // 60 seconds in a minute
+  const remainingSeconds = seconds % 60;
+
+  let result = "";
+
+  if (days > 0) {
+      result += `${days}d `;
+  }
+
+  if (hours > 0 || days > 0) {
+      result += `${hours}h `;
+  }
+
+  if (minutes > 0 || hours > 0 || days > 0) {
+      result += `${minutes}m `;
+  }
+
+  result += `${remainingSeconds}s`;
+
+  return result.trim(); // Remove leading/trailing whitespaces
 }
 
 function checkInstances() {
@@ -52,20 +85,57 @@ function checkInstances() {
     .getGameInstances(placeId)
     .then((instances) => {
       const instanceCount = instances.length;
-      if (instanceCount === 0) {
-            const channel = client.channels.cache.get('1202020061221761165');   // Replace with your channel ID
-            channel.send(`There are no instances open for Elysium (${counter}).`);   // shows placeID - channel.send(`There are no instances open for place ID ${placeId}.`);
-            counter++;
-          } else {
-            const channel = client.channels.cache.get('1202020061221761165');   // Replace with your channel ID
-            channel.send(`<@254344094636179466> There are ${instanceCount} instances open for Elysium.`);   // shows placeID - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
-          }
-        }).catch(err => {
-          console.error(err);
-        });
+      // More than one server
+      if (instanceCount > 0) {
+        const channel = client.channels.cache.get('1202020061221761165');   // GET channel ID
+        // When server instances increase
+        if (instance_counter_tracker < instanceCount) {                     
+          channel.send(`<@&1202351752574423050> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${formatTime(yes_counter)},   Total: ${formatTime(total_counter)})`);   // shows placeID - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
+          instance_counter_tracker = instanceCount;
+          total_counter++;
+          yes_counter++;
+          no_counter = 1;
+        // When server instances decrease
+        } else if (instance_counter_tracker > instanceCount) {                     
+          channel.send(`<@&1202351752574423050> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${formatTime(yes_counter)},   Total: ${formatTime(total_counter)})`);   // shows placeID - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
+          instance_counter_tracker = instanceCount;
+          total_counter++;
+          yes_counter++;
+          no_counter = 1;
+        // When server instance stays the same
+        } else {
+          channel.send(`Elysium has **${instanceCount}** instance${instanceCount !== 1 ? 's' : ''} open   (Uptime: ${formatTime(yes_counter)}  |  Total: ${formatTime(total_counter)})`);
+          total_counter++;
+          yes_counter++;
+          no_counter = 1; // Reset how long there has been no servers
+        }
+      // One server
+      } else if (instanceCount === 0) {
+        const channel = client.channels.cache.get('1202020061221761165');   // GET channel ID
+        channel.send(`No open instances for Elysium   (None: ${formatTime(no_counter)}   Total: ${formatTime(total_counter)})`);   // shows placeID - channel.send(`There are no instances open for place ID ${placeId}.`);
+        instance_counter_tracker = 0;
+        total_counter++;
+        no_counter++;
+        yes_counter = 1; // Reset how long there has been a server up
       }
+
+      // Counters reset after 10 days if run constantly (unlikely). Only total counter will reset after 10 days all the time.
+      if (total_counter === 8640) {
+        total_counter = 1;
+      //  total_days_counter++;
+      } else if (no_counter === 8640) {
+        no_counter = 1;
+      } else if (yes_counter === 8640) {
+        yes_counter = 1;
+      }
+    }).catch(err => {
+      console.error(err);
+    });
+  }
 
 setInterval(checkInstances, 10000); // Milliseconds
 
 client.login(process.env.TOKEN);
+
+
 
