72028b8ca7e789fed95f42340d66ac904b8b1a6d
Jason Kong <jjkong149@hotmail.com>
2024-02-02 02:16:45 -0500

    added let variables, replaced time tracking through counters with Date.now(), organized and cleaned up code

--- src/index.js
+++ src/index.js
@@ -41,99 +41,128 @@ client.on("messageCreate", async (message) => {
   }
 });
 
-// Counters
-total_counter = 1; // Total bot uptime
 
-no_counter = 1; // Time that there is no server
+// Time Elapsed Functions
+function totalTimeElapsed(total_end) {
+  let elapsed = total_end - total_start;  // elapsed time in milliseconds
+  return formatTime(elapsed/1000); // convert milliseconds to seconds
+}
 
-yes_counter = 1; // Time that there is a server
+function yesTimeElapsed(yes_end) {
+  let elapsed = yes_end - yes_start;  // elapsed time in milliseconds
+  return formatTime(elapsed/1000); // convert milliseconds to seconds
+}
 
-// total_days_counter = 0; // Total days that bot is up
+function noTimeElapsed(no_end) {
+  let elapsed = no_end - no_start;  // elapsed time in milliseconds
+  return formatTime(elapsed/1000); // convert milliseconds to seconds
+}
 
-instance_counter_tracker = 0; // stores instance value
-
-// Formats the seconds*10 into d, h, m, s
-function formatTime(totalSeconds) {
-  const seconds = totalSeconds * 10;
+// Formats the seconds into d, h, m, s
+function formatTime(seconds) {
+//  const seconds = totalSeconds;
   const days = Math.floor(seconds / 864000); // 864000 seconds in a day
   const hours = Math.floor((seconds % 864000) / 3600); // 3600 seconds in an hour
-  const minutes = Math.floor((seconds % 3600) / 60); // 60 seconds in a minute
-  const remainingSeconds = seconds % 60;
+  let minutes = Math.floor((seconds % 3600) / 60); // 60 seconds in a minute
+  let remainingSeconds = Math.round(seconds % 60);
+  if (remainingSeconds === 60) {
+    minutes += 1;
+    remainingSeconds = 0;
+  }
 
   let result = "";
 
   if (days > 0) {
       result += `${days}d `;
   }
-
   if (hours > 0 || days > 0) {
       result += `${hours}h `;
   }
-
   if (minutes > 0 || hours > 0 || days > 0) {
       result += `${minutes}m `;
   }
-
   result += `${remainingSeconds}s`;
 
   return result.trim(); // Remove leading/trailing whitespaces
 }
 
+
+// Counters
+let total_counter = 1; // Total bot uptime
+let no_counter = 1; // Time that there is no server
+let yes_counter = 1; // Time that there is a server
+
+let instance_counter_tracker = 0; // stores instance value
+
+let total_start = Date.now(); 
+let total_end = Date.now();
+let no_start = Date.now();
+let yes_start = Date.now();
+
+
+// Main
 function checkInstances() {
   const placeId = "14894612329"; // Replace with your place ID
   noblox
     .getGameInstances(placeId)
     .then((instances) => {
       const instanceCount = instances.length;
+
+      total_end = Date.now(); // 0s first run.
+
       // More than one server
       if (instanceCount > 0) {
+        if (yes_counter === 1) {
+          yes_start = Date.now();
+          yes_counter = null;
+          no_counter = 1;  // Reset counter for tracking start time of having no servers (only for no_start)
+        }
+        if (total_counter === 1) {
+          total_start = Date.now();
+        }
+        no_end = Date.now();
         const channel = client.channels.cache.get('1202020061221761165');   // GET channel ID
         // When server instances increase
-        if (instance_counter_tracker < instanceCount) {                     
-          channel.send(`<@&1202351752574423050> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${formatTime(yes_counter)},   Total: ${formatTime(total_counter)})`);   // shows placeID - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
+        if (instance_counter_tracker < instanceCount) { 
+          yes_end = Date.now(); // Could place only one above but put here for accurate times                   
+          channel.send(`<@&1202351752574423050> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${yesTimeElapsed(yes_end)},   Total: ${totalTimeElapsed(total_end)})`);   // shows placeID - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
           instance_counter_tracker = instanceCount;
           total_counter++;
-          yes_counter++;
-          no_counter = 1;
         // When server instances decrease
-        } else if (instance_counter_tracker > instanceCount) {                     
-          channel.send(`<@&1202351752574423050> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${formatTime(yes_counter)},   Total: ${formatTime(total_counter)})`);   // shows placeID - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
+        } else if (instance_counter_tracker > instanceCount) {
+          yes_end = Date.now();                     
+          channel.send(`<@&1202351752574423050> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${yesTimeElapsed(yes_end)},   Total: ${totalTimeElapsed(total_end)})`);   // shows placeID - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
           instance_counter_tracker = instanceCount;
           total_counter++;
-          yes_counter++;
-          no_counter = 1;
         // When server instance stays the same
         } else {
-          channel.send(`Elysium has **${instanceCount}** instance${instanceCount !== 1 ? 's' : ''} open   (Uptime: ${formatTime(yes_counter)}  |  Total: ${formatTime(total_counter)})`);
+          yes_end = Date.now()
+          channel.send(`Elysium has **${instanceCount}** instance${instanceCount !== 1 ? 's' : ''} open    (Uptime: ${yesTimeElapsed(yes_end)}  |  Total: ${totalTimeElapsed(total_end)})`);
           total_counter++;
-          yes_counter++;
-          no_counter = 1; // Reset how long there has been no servers
         }
+
       // One server
       } else if (instanceCount === 0) {
+        if (no_counter === 1) {
+          no_start = Date.now();
+          no_counter = null;
+          yes_counter = 1;  // Reset counter for tracking start time of an up server (only for yes_start)
+        }
+        if (total_counter === 1) {
+          total_start = Date.now();
+        }
+        yes_end = Date.now();
         const channel = client.channels.cache.get('1202020061221761165');   // GET channel ID
         if (instance_counter_tracker > 0) {
-          channel.send(`<@&1202351752574423050> All Elysiums have shut down   (Uptime: ${formatTime(yes_counter)}  |  Total: ${formatTime(total_counter)})`);
-          instance_counter_tracker = 0;
+          channel.send(`<@&1202351752574423050> All Elysiums have shut down   (Uptime: ${yesTimeElapsed(yes_end)}  |  Total: ${totalTimeElapsed(total_end)}`);
+          instance_counter_tracker = 0;   // Reset counter because only will only be used when server goes back up
           total_counter++;
-          no_counter++;
-          yes_counter = 1; // Reset how long there has been a server up
         } else {
-          channel.send(`No open instances for Elysium   (None: ${formatTime(no_counter)}   Total: ${formatTime(total_counter)})`);   // shows placeID - channel.send(`There are no instances open for place ID ${placeId}.`);
+          no_end = Date.now();
+          channel.send(`No open instances for Elysium   (None: ${noTimeElapsed(no_end)}  |  Total: ${totalTimeElapsed(total_end)})`);   // shows placeID - channel.send(`There are no instances open for place ID ${placeId}.`);
           total_counter++;
-          no_counter++;
         }
       }
-
-      // Counters reset after 1 days if run constantly (unlikely). Only total counter will reset after 1 days all the time.
-      // if (total_counter === 8640) {
-      //   total_counter = 1;
-      // //  total_days_counter++;
-      // } else if (no_counter === 8640) {
-      //   no_counter = 1;
-      // } else if (yes_counter === 8640) {
-      //   yes_counter = 1;
-      // }
     }).catch(err => {
       console.error(err);
     });
