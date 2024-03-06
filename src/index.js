081a4f816c5c48a18d7476a873c5687a63501720
Jason Kong <jjkong149@hotmail.com>
2024-03-06 01:31:41 -0500

    fixed output messages

--- src/index.js
+++ src/index.js
@@ -173,12 +173,12 @@ async function checkInstances() {
         // When server instances increase
         if (instance_counter_tracker < instanceCount) { 
           yes_end = Date.now(); // Could place only one above but put here for accurate times
-          channel.send(`<@&${roleID}> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${yesTimeElapsed(yes_end)},   Total: ${totalTimeElapsed(total_end)})`);   // shows placeId - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
+          channel.send(`<@&${roleID}> There are **${instanceCount}** instance(s) open for Elysium   (Server: ${yesTimeElapsed(yes_end)},   Bot: ${totalTimeElapsed(total_end)})`);   // shows placeId - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
           instance_counter_tracker = instanceCount;
         // When server instances decrease
         } else if (instance_counter_tracker > instanceCount) {
           yes_end = Date.now();                     
-          channel.send(`<@&${roleID}> There are **${instanceCount}** instance(s) open for Elysium   (Uptime: ${yesTimeElapsed(yes_end)},   Total: ${totalTimeElapsed(total_end)})`);   // shows placeId - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
+          channel.send(`<@&${roleID}> There are **${instanceCount}** instance(s) open for Elysium   (Server: ${yesTimeElapsed(yes_end)},   Bot: ${totalTimeElapsed(total_end)})`);   // shows placeId - channel.send(`<@254344094636179466> There are ${instanceCount} instances open for place ID ${placeId}.`);
           instance_counter_tracker = instanceCount;
         // When server instance stays the same
         } else {
@@ -200,7 +200,7 @@ async function checkInstances() {
         }
         yes_end = Date.now();
         if (instance_counter_tracker > 0) {
-          channel.send(`<@&${roleID}> All Elysiums have shut down   (Uptime: ${yesTimeElapsed(yes_end)}  |  Total: ${totalTimeElapsed(total_end)}`);
+          channel.send(`<@&${roleID}> All Elysiums have shut down   (No Servers: ${yesTimeElapsed(yes_end)}  |  Bot: ${totalTimeElapsed(total_end)}`);
           instance_counter_tracker = 0;   // Reset counter because only will only be used when server goes back up
         } else {
           no_end = Date.now();
