0d523534a67da77d4e414f73c775f4a575236e79
Jason Kong <jjkong149@hotmail.com>
2024-03-06 01:23:11 -0500

    removed bot spamming messages will only ping, added time command

--- src/index.js
+++ src/index.js
@@ -27,22 +27,40 @@ client.on("ready", (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
 });
  
+// client.on("messageCreate", async (message) => {
+//   if (message.content.startsWith("check")) {
+//     const placeId = message.content.split(" ")[1];
+//     const instances = await noblox.getGameInstances(placeId);
+//     let instanceCount = instances.length;
+//     if (instanceCount === 0) {
+//       message.reply(`There are no instances open for place ID ${placeId}.`);
+//     } else {
+//       message.reply(
+//         `There are ${instanceCount} instances open for place ID ${placeId}.`,
+//       );
+//     }
+//   }
+// });
+
+let lastExecutionTime = 0; // Initialize with zero (no previous execution)
+
 client.on("messageCreate", async (message) => {
-  if (message.content.startsWith("check")) {
-    const placeId = message.content.split(" ")[1];
-    const instances = await noblox.getGameInstances(placeId);
-    const instanceCount = instances.length;
-    if (instanceCount === 0) {
-      message.reply(`There are no instances open for place ID ${placeId}.`);
-    } else {
-      message.reply(
-        `There are ${instanceCount} instances open for place ID ${placeId}.`,
-      );
+  if (message.content.startsWith("time")) {
+    let currentTime = Date.now() / 1000;
+    if (currentTime - lastExecutionTime >= 5) {
+      const instances = await noblox.getGameInstances(placeId);
+      let instanceCount = instances.length;
+      if (instanceCount > 0) {
+          message.reply(`Server Uptime: ${time2}  |  Bot Uptime: ${total_time2}`);
+          lastExecutionTime = currentTime;
+      } else {
+          message.reply(`No Servers: ${time2}  |  Bot Uptime: ${total_time2}`);
+          lastExecutionTime = currentTime;
+      } 
     }
   }
 });
- 
- 
+
 // Time Elapsed Functions
 function totalTimeElapsed(total_end) {
   let elapsed = total_end - total_start;  // elapsed time in milliseconds
@@ -97,7 +115,13 @@ function formatTime(seconds) {
   return result;
 }
 
- 
+// Store time
+function currentTime(time, total_time, channel) {
+  time2 = time;
+  total_time2 = total_time;
+  channel2 = channel;
+}
+
 // Counters
 let total_counter = 1; // Used to track total_start
 let no_counter = 1; // Used to track no_start
@@ -109,12 +133,14 @@ let total_start = Date.now();
 let total_end = Date.now();
 let no_start = Date.now();
 let yes_start = Date.now();
- 
- 
+
+const placeId = "14894612329"; // GET place ID
+const roleID = "1202351752574423050"; // GET role ID
+
+bot_start = true;
+
 // Main
 async function checkInstances() {
-  const placeId = "14894612329"; // GET place ID
-  const roleID = "1202351752574423050"; // GET role ID
   const channel = await client.channels.fetch('1202020061221761165');   // GET channel ID
   // console.log(`The role ID is: ${roleID}`);
   // console.log(`The place ID is: ${placeId}`);
@@ -123,9 +149,15 @@ async function checkInstances() {
     .getGameInstances(placeId)
     .then((instances) => {
       const instanceCount = instances.length;
- 
+      
+      if (bot_start == true) {
+        currentTime(null, null, 1202020061221761165);
+        channel.send("The Elysium Bot is now up. Server uptime will be inaccurate if there is an Elysium.")
+        bot_start = false;
+      }
+
       total_end = Date.now(); // 0s first run.
- 
+
       // More than one server
       if (instanceCount > 0) {
         if (yes_counter === 1) {
@@ -151,7 +183,8 @@ async function checkInstances() {
         // When server instance stays the same
         } else {
           yes_end = Date.now()
-          channel.send(`Elysium has **${instanceCount}** instance${instanceCount !== 1 ? 's' : ''} open    (Uptime: ${yesTimeElapsed(yes_end)}  |  Total: ${totalTimeElapsed(total_end)})`);
+          currentTime(yesTimeElapsed(yes_end), totalTimeElapsed(total_end), channel)
+          // channel.send(`Elysium has **${instanceCount}** instance${instanceCount !== 1 ? 's' : ''} open    (Uptime: ${yesTimeElapsed(yes_end)}  |  Total: ${totalTimeElapsed(total_end)})`);
         }
  
       // One server
@@ -171,13 +204,14 @@ async function checkInstances() {
           instance_counter_tracker = 0;   // Reset counter because only will only be used when server goes back up
         } else {
           no_end = Date.now();
-          channel.send(`No open instances for Elysium   (None: ${noTimeElapsed(no_end)}  |  Total: ${totalTimeElapsed(total_end)})`);   // shows placeId - channel.send(`There are no instances open for place ID ${placeId}.`);
+          currentTime(noTimeElapsed(yes_end), totalTimeElapsed(total_end), channel)
+          // channel.send(`No open instances for Elysium   (None: ${noTimeElapsed(no_end)}  |  Total: ${totalTimeElapsed(total_end)})`);   // shows placeId - channel.send(`There are no instances open for place ID ${placeId}.`);
         }
       }
     }).catch(err => {
       console.error(err);
     });
   }
-setInterval(checkInstances, 30000); // Milliseconds
+setInterval(checkInstances, 5000); // Milliseconds
  
 client.login(process.env.TOKEN);
